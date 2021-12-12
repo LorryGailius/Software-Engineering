a8ebac7a68dac509e9541580302fd7d906295a98
Laurynas Gailius <lauris.gailius@gmail.com>
2021-12-12 11:54:49 +0200

    Update 8.4.c

--- Course 1/Procedular Programming/Educational_Tasks/8/8.4.c
+++ Course 1/Procedular Programming/Educational_Tasks/8/8.4.c
@@ -6,23 +6,6 @@ int splitData(int arr[], int size, int size1, int **arr1,int **arr2);
 
 int main()
 {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10}, size = 10, size1 = 4;
-    int *r1,*r2;
-
-    splitData(arr,size,size1,&r1,&r2);
-
-    for (int i = 0; i < size1; i++)
-    {
-        printf("%d ",r1[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < size-size1; i++)
-    {
-        printf("%d ",r2[i]);
-    }
-    
-    free(r1);
-    free(r2);
 
     return 0;
 }
